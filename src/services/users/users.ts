b787134567af79c142577c394f202c2db085f3b2
wilson nie <wilson.nie13@gmail.com>
2021-09-02 10:45:48 -0700

    uhmmm wat

--- src/services/users/users.ts
+++ src/services/users/users.ts
@@ -3,18 +3,18 @@ import * as config from "../../secret.json";
 import axios from "axios";
 
 export default class Users {
-    // logIn(email: string, password: string) {
-    //      let data = {
-    //         "user_login_data": {
-    //             "username": email,
-    //             "password": password
-    //         }
-    //     }
-    //     axios.post(`${config.baseURL}/users/auth/login`, data).then((res: any) => {
-    //         console.log(res)
-    //     })
-    // }
-    add(a, b){
+    logIn(email: string, password: string) {
+         let data = {
+            "user_login_data": {
+                "username": email,
+                "password": password
+            }
+        }
+        axios.post(`${config.baseURL}/users/auth/login`, data).then((res: any) => {
+            console.log(res)
+        })
+    }
+    add(a: number, b: number){
         return a + b
     }
 }
