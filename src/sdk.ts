fd9498f970be2450b2707735529d88259d70f22e
wilson nie <wilson.nie13@gmail.com>
2021-09-08 17:09:05 -0700

    removed test code

--- src/sdk.ts
+++ src/sdk.ts
@@ -35,21 +35,6 @@ export class eHelplySDK {
         this.axiosClient.defaults.headers.common["x-access-token"] = accessToken;
 
     }
-
 }
-let ehelplyClient: eHelplySDK = new eHelplySDK();
-ehelplyClient.setAccessToken("86_s5ExYx9GsIXuUd18857d6d16811eb97cb0a58a9feac02xYx9GsIXuU2im-hB");
-ehelplyClient.setSecretToken("yckQy9fzAh6oNFi-3f40ea9dc5294f2a92484e95e2179fb7GFHyckQy9fzAh6oN");
-ehelplyClient.setProjectUuid("8278b088-d167-11eb-a712-0a58a9feac02");
-let payload: createReviewRequest = {
-    review: {review_text: "pls work", max_rating: 10, rating: 5}
-}
-let payload2: updateReviewRequest = {
-    review: {review_text: "updated text work pls", rating: 5}
-}
-ehelplyClient.reviewSdk.create(payload,"type_name", "uuid1234")
-ehelplyClient.reviewSdk.get("446fd74c-1039-11ec-a7cc-0a58a9feac02", "type_name", "uuid1234")
-ehelplyClient.reviewSdk.update(payload2, "446fd74c-1039-11ec-a7cc-0a58a9feac02", "type_name", "uuid1234")
-ehelplyClient.reviewSdk.get("446fd74c-1039-11ec-a7cc-0a58a9feac02", "type_name", "uuid1234")
-// ehelplyClient.reviewSdk.delete("446fd74c-1039-11ec-a7cc-0a58a9feac02", "type_name", "uuid1234")
-// export default ehelplyClient;
+declare const ehelplyClient: eHelplySDK;
+export default ehelplyClient;
--- src/services/products/reviews/reviews.ts
+++ src/services/products/reviews/reviews.ts
@@ -8,29 +8,21 @@ export default class ReviewSdk {
   }
   create(payload: createReviewRequest, type: string, entity: string ): void {
     this.axiosClient.post(`/products/reviews/types/${type}/entities/${entity}`, payload).then((res: any) => {
-      console.log("create")
-      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       console.log(res.data)
     })
   }
   update(payload: updateReviewRequest, reviewUuid: string, type: string, entity: string ): void{
     this.axiosClient.put(`/products/reviews/types/${type}/entities/${entity}/reviews/${reviewUuid}`, payload).then((res: any) => {
-      console.log("update")
-      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       console.log(res.data)
     })
   }
   get(reviewUuid: string, type: string, entity: string ){
     this.axiosClient.get(`/products/reviews/types/${type}/entities/${entity}/reviews/${reviewUuid}`).then((res: any) => {
-      console.log("get")
-      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       console.log(res.data)
     })
 
   }
   delete(reviewUuid: string, type: string, entity: string){
-    console.log("delete")
-    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     this.axiosClient.delete(`/products/reviews/types/${type}/entities/${entity}/reviews/${reviewUuid}`).then((res: any) => {
       console.log(res.data)
     })
