709811e0909d298aee08c5178ad3b535194ef064
Shawn Clake <shawn.clake@gmail.com>
2021-11-11 09:27:04 -0600

    Removed strange and unnessecary sub files

--- src/services/products/reviews/reviews.d.ts
+++
@@ -1,9 +0,0 @@
-import { createReviewRequest, updateReviewRequest } from "./reviewTypes";
-export default class ReviewSdk {
-    axiosClient: any;
-    constructor(axiosClient: any);
-    create(payload: createReviewRequest, type: string, entity: string): void;
-    update(payload: updateReviewRequest, reviewUuid: string, type: string, entity: string): void;
-    get(reviewUuid: string, type: string, entity: string): void;
-    delete(reviewUuid: string, type: string, entity: string): void;
-}
--- src/services/users/users.d.ts
+++
@@ -1,5 +0,0 @@
-export default class UserSdk {
-    axiosClient: any;
-    constructor(axiosClient: any);
-    logIn(email: string, password: string): void;
-}
