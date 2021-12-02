1e531101ca0d96bc7062a7f8d722ffb9944b9238
wilson nie <wilson.nie13@gmail.com>
2021-12-02 05:24:11 -0800

    added billing endpoint still need some refactoring and testing to be done

--- src/services/products/billing/billing.ts
+++ src/services/products/billing/billing.ts
@@ -11,6 +11,7 @@ export default class BillingSdk {
     this.axiosClient = axiosClient;
     this.logger = logger;
   }
+
   createBillingAccount(): Promise<BillingAccountResponse> {
     return this.axiosClient.post<BillingAccountResponse>(
       `/products/billing/create_billing_account`
