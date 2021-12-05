11c391164cb2c45c90fbf8317d8f4237916cf9b4
wilson nie <wilson.nie13@gmail.com>
2021-12-05 06:16:49 -0800

    moving resposne and requests types into their own file

--- src/services/sam/supports/types/ticketReponseTypes.ts
+++ src/services/sam/supports/types/ticketReponseTypes.ts
@@ -1,16 +1,3 @@
-export interface CreateTicketRequest {
-  ticket: {
-    subject: string,
-    priority: string,
-  }
-}
-export interface CreateContactRequest {
-  email: string,
-  first_name: string,
-  last_name: string,
-  phone_number: string
-}
-
 export interface CreateTicketResponse {
   ticket_id: string,
   subject: string,
