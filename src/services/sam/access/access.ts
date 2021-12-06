516c868bf081ac5f2a029083d3ebbea854191807
wilson nie <wilson.nie13@gmail.com>
2021-12-06 07:07:07 -0800

    added get key endpoints

---
+++ src/services/sam/access/access.ts
@@ -0,0 +1,22 @@
+import {Logger} from "../../../utils/logger";
+import {AxiosInstance, AxiosResponse} from "axios";
+import {GetKeyResponse} from "./types/accessResponseTypes";
+
+export default class AccessSdk {
+  axiosClient: AxiosInstance
+  logger: Logger
+
+  constructor(axiosClient: AxiosInstance, logger: Logger) {
+    this.axiosClient = axiosClient;
+    this.logger = logger;
+  }
+
+  getKey(keyUuid: string): Promise<GetKeyResponse> {
+    return this.axiosClient.post<GetKeyResponse>(
+      `/sam/security/keys/${keyUuid}`
+    ).then((res: AxiosResponse<GetKeyResponse>) => {
+      this.logger.debug(res);
+      return res.data;
+    });
+  }
+}
