292ad584ded8cc8ee462878771d2aa7d2e28441e
wilson nie <wilson.nie13@gmail.com>
2021-12-06 08:39:18 -0800

    adding response model for getkey function

--- src/services/sam/access/types/accessResponseTypes.ts
+++ src/services/sam/access/types/accessResponseTypes.ts
@@ -1,5 +1,10 @@
 export interface GetKeyResponse {
-
+  uuid: string,
+  access: string,
+  name: string,
+  summary: string,
+  created_at: string,
+  last_used_at: string
 }
 
 export interface GetAccessibilityResponse{
