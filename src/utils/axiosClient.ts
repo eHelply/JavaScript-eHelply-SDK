3d6bccf7912d1124a0a0e84b3126b086368adc8a
SdkBot <business@ehelply.com>
2021-12-08 09:16:53 -0600

    TypeScript SDK 1.1.30

--- src/utils/axiosClient.ts
+++
@@ -1,8 +0,0 @@
-import {AxiosInstance, default as axios} from "axios";
-
-export function makeAxiosClientHeaderless(original: AxiosInstance): AxiosInstance {
-    return axios.create({
-        timeout: original.defaults.timeout,
-        baseURL: original.defaults.baseURL
-    });
-}
