32e1061c1dbec9f0c70f5759df3d072b2f9c1115
wilson nie <wilson.nie13@gmail.com>
2022-02-09 11:12:07 -0800

    adding more security endpoints

--- src/services/sam/security/security.ts
+++ src/services/sam/security/security.ts
@@ -1,6 +1,7 @@
 import {Logger} from "../../../utils/logger";
 import {AxiosInstance, AxiosResponse} from "axios";
-import { SearchSecurityKeysResponse } from "./types/securityResponseTypes";
+import { GetSecurityKeyResponse } from "./types/securityResponseTypes";
+import { PostSecurityKeyVerifyRequest, PostCreateSecurityKeyRequest } from "./types/securityRequestTypes";
 
 export default class SecuritySdk {
   axiosClient: AxiosInstance
@@ -10,22 +11,33 @@ export default class SecuritySdk {
     this.axiosClient = axiosClient;
     this.logger = logger;
   }
-  searchSecurityKeys (): Promise<Array<SearchSecurityKeysResponse>> {
-    return this.axiosClient.get<Array<SearchSecurityKeysResponse>>(
-      `/sam/security/keys`,
-      payload
-    ).then((res: AxiosResponse<Array<SearchSecurityKeysResponse>>) => {
+  searchSecurityKeys (): Promise<Array<GetSecurityKeyResponse>> {
+    return this.axiosClient.get<Array<GetSecurityKeyResponse>>(
+      `/sam/security/keys`
+    ).then((res: AxiosResponse<Array<GetSecurityKeyResponse>>) => {
       this.logger.debug(res);
       return res.data;
     });
   }
 
-  getSecurityKey () {
-
+  getSecurityKey (keyUuid: string): Promise<GetSecurityKeyResponse> {
+    return this.axiosClient.get<GetSecurityKeyResponse>(
+      `/sam/security/keys/${keyUuid}`
+    ).then((res: AxiosResponse<GetSecurityKeyResponse>) => {
+      this.logger.debug(res);
+      return res.data;
+    });
   }
 
-  postSecurityKeyVerify () {
 
+  postSecurityKeyVerify (payload: PostSecurityKeyVerifyRequest): Promise<GetSecurityKeyResponse> {
+    return this.axiosClient.post<GetSecurityKeyResponse>(
+      `/sam/security/keys/verify`,
+      payload
+    ).then((res: AxiosResponse<GetSecurityKeyResponse>) => {
+      this.logger.debug(res);
+      return res.data;
+    });
   }
 
   postCreateSecurityKey () {
--- src/services/sam/security/types/securityResponseTypes.ts
+++ src/services/sam/security/types/securityResponseTypes.ts
@@ -1,4 +1,4 @@
-export interface SearchSecurityKeysResponse {
+export interface SecurityKey {
   uuid: string,
   access: string,
   name: string,
@@ -6,3 +6,5 @@ export interface SearchSecurityKeysResponse {
   created_at: string,
   last_used_at: string,
 }
+
+export interface GetSecurityKeyResponse extends SecurityKey {}
